d408586fb90241f8d490f4c538f08e3b9c7caec9
ItsEternity <ItsEternity@users.noreply.github.com>
2016-08-05 21:19:05 +0100

    Update models.py

--- ticker/models.py
+++ ticker/models.py
@@ -9,20 +9,13 @@ SERVER_CHOICES = (
     ('UHC2', 'Ultra Hardcore Server 2'),
 )
 
-GAMEMODE_CHOICES = (
-    ('UHC', 'Ultra Hardcore'),
-    ('SUR', 'Survival'),
-)
-
-
 class Event(models.Model):
     name = models.CharField(max_length=45)
-    host = models.CharField(max_length=40)
     date = models.DateField(default=timezone.now)
     start_time = models.TimeField(default=datetime.time(hour=0, minute=0))
     end_time = models.TimeField(blank=True, null=True)
     server = models.CharField(choices=SERVER_CHOICES, default='UHC1', max_length=4)
-    gamemode = models.CharField(choices=GAMEMODE_CHOICES, default='UHC', max_length=3)
+    gamemode = models.CharField(default='Vanilla', max_length=100)
     team_size = models.IntegerField(default=1)
     notes = notes = models.TextField(blank=True, null=True)
 
